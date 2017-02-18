c50f93517dbae1f49c2016220b4a8f5a680f2df6
Ajit Singh <Ajit.Singh3@mindtree.com>
2017-02-18 21:53:46 +0530

    Automated Push

--- bot/intents/hotel.js
+++ bot/intents/hotel.js
@@ -6,7 +6,14 @@ var bookRoom = function(session, args, next, builder) {
     location = builder.EntityRecognizer.findEntity(args.entities, 'location');
     dates = builder.EntityRecognizer.findAllEntities(args.entities, 'builtin.datetime.date');
     rating = builder.EntityRecognizer.findEntity(args.entities, 'rating');
-
+    var card = new builder.HeroCard(session)
+        .title("Hotel Residency")
+        .text("Hotel Residency is a lot awesome.")
+        .images([
+            builder.CardImage.create(session, "https://project-xenia-images.herokuapp.com/fab-normal.png")
+        ]);
+    var msg = new builder.Message(session).attachments([card]);
+    session.send(msg);
     if (!location) {
         console.log("Location not present");
         builder.Prompts.text(session, "Please enter the city ?");
@@ -17,14 +24,6 @@ var bookRoom = function(session, args, next, builder) {
 var _bookRoom = function(session, results, builder) {
     console.log(builder);
     location = results.response;
-    var card = new builder.HeroCard(session)
-        .title("Hotel Residency")
-        .text("Hotel Residency is a lot awesome.")
-        .images([
-            builder.CardImage.create(session, "https://project-xenia-images.herokuapp.com/fab-normal.png")
-        ]);
-    var msg = new builder.Message(session).attachments([card]);
-    session.send(msg);
     console.log("You asked for", location);
     session.send("You asked for " + location);
     // if (dates) {
